3a08d42d95eaafedf7306d2854034e3fc9f12f05
Aidan De Angelis <angaidan@amazon.com>
2023-11-08 14:18:30 -0800

    Remove unused colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,43 +1,9 @@
 return {
   {
     "catppuccin/nvim",
-    main = "catppuccin",
+    name = "catppuccin",
     lazy = false,
     priority = 1000,
-    opts = {
-      integrations = {
-        aerial = true,
-        bufferline = true,
-        dashboard = true,
-        fidget = true,
-        flash = true,
-        gitsigns = true,
-        harpoon = true,
-        headlines = true,
-        indent_blankline = { enabled = true },
-        mason = true,
-        mini = { enabled = true },
-        neotree = true,
-        neogit = true,
-        noice = true,
-        cmp = true,
-        native_lsp = {
-          enabled = true,
-          inlay_hints = { background = "true" },
-        },
-        notify = true,
-        semantic_tokens = true,
-        treesitter_context = true,
-        treesitter = true,
-        telescope = { enabled = true },
-        lsp_trouble = true,
-        illuminate = {
-          enabled = true,
-          lsp = true,
-        },
-        which_key = true,
-      },
-    },
   },
   {
     "folke/tokyonight.nvim",
@@ -54,18 +20,8 @@ return {
     },
   },
   {
-    "nyoom-engineering/oxocarbon.nvim",
-    lazy = false,
-    priority = 1000,
-  },
-  {
     "rose-pine/neovim",
     name = "rose-pine",
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    "maxmx03/fluoromachine.nvim",
     lazy = false,
     priority = 1000,
   },
@@ -87,19 +43,5 @@ return {
     "EdenEast/nightfox.nvim",
     lazy = false,
     priority = 1000,
-  },
-  {
-    "justinsgithub/oh-my-monokai.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      transparent_background = true,
-      ---@param c Colorscheme
-      override = function(c)
-        return {
-          LspInlayHint = { fg = c.specialcomment },
-        }
-      end,
-    },
   },
 }

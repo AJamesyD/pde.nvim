72f45144c336ebf08fda7a6169f9b9a2b3e97325
Aidan De Angelis <angaidan@amazon.com>
2024-04-17 10:53:44 -0700

    Remove unnecessary colorscheme config

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -22,8 +22,6 @@ return {
       on_highlights = function(hl, c)
         hl.CursorLineNr = { fg = c.orange, bold = true }
         hl.LineNr = { fg = c.orange, bold = true }
-        hl.LineNrAbove = { fg = c.fg_gutter }
-        hl.LineNrBelow = { fg = c.fg_gutter }
       end,
     },
     config = true,

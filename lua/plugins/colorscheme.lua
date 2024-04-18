b4b6fe65b3a0189e97b4e94c23b3c086b80486c8
Aidan De Angelis <angaidan@amazon.com>
2024-04-18 13:02:33 -0700

    Revert "Remove unnecessary colorscheme config"
    This reverts commit 72f45144c336ebf08fda7a6169f9b9a2b3e97325.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -22,6 +22,8 @@ return {
       on_highlights = function(hl, c)
         hl.CursorLineNr = { fg = c.orange, bold = true }
         hl.LineNr = { fg = c.orange, bold = true }
+        hl.LineNrAbove = { fg = c.fg_gutter }
+        hl.LineNrBelow = { fg = c.fg_gutter }
       end,
     },
     config = true,

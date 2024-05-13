7fa8a6e17089e6e1570197287fb87c1bdde0bd00
Aidan De Angelis <angaidan@amazon.com>
2024-05-13 15:03:59 -0700

    Temporarily disable autocmd to disable rnu in cmdline

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -5,23 +5,25 @@ local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
 
 -- Show absolute line numbers in cmd mode only
-autocmd("CmdlineEnter", {
-  desc = "Absolute numbers in cmd mode",
-  group = augroup("cmdline_enter", { clear = true }),
-  callback = function()
-    vim.opt.relativenumber = false
-  end,
-})
-
-autocmd("CmdlineLeave", {
-  desc = "Relative numbers outside cmd mode",
-  group = augroup("cmdline_leave", { clear = true }),
-  callback = function()
-    if vim.wo.number then
-      vim.opt.relativenumber = true
-    end
-  end,
-})
+-- autocmd("CmdlineEnter", {
+--   desc = "Absolute numbers in cmd mode",
+--   group = augroup("cmdline_enter", { clear = true }),
+--   callback = function()
+--     if vim.wo.number then
+--       vim.wo.relativenumber = false
+--     end
+--   end,
+-- })
+--
+-- autocmd("CmdlineLeave", {
+--   desc = "Relative numbers outside cmd mode",
+--   group = augroup("cmdline_leave", { clear = true }),
+--   callback = function()
+--     if vim.wo.number then
+--       vim.wo.relativenumber = true
+--     end
+--   end,
+-- })
 
 --- Filetype specific options
 autocmd("FileType", {

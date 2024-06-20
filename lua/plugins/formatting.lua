fbecba50822258ec3365824ff61299957934d848
Aidan De Angelis <angaidan@amazon.com>
2024-06-20 13:45:21 -0700

    Update treesj max fold length

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -25,6 +25,9 @@ return {
     keys = {
       { "<leader>m", "<CMD>TSJToggle<CR>", desc = "Toggle split/join" },
     },
-    opts = { use_default_keymaps = false },
+    opts = {
+      use_default_keymaps = false,
+      max_join_length = 240,
+    },
   },
 }

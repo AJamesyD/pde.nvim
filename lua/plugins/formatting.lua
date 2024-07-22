9fa59507085a39c7ae9e63705cd9590001a56536
Aidan De Angelis <angaidan@amazon.com>
2024-07-22 09:55:20 -0700

    Remove conform config in favor of lazyvim defaults

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,26 +1,5 @@
 return {
   {
-    "stevearc/conform.nvim",
-    ---@type ConformOpts
-    opts = {
-      format = {
-        lsp_fallback = "always",
-      },
-      formatters_by_ft = {
-        python = function(bufnr)
-          if require("conform").get_formatter_info("black", bufnr).available then
-            return { "black" }
-          else
-            return { "ruff_fix", "ruff_format" }
-          end
-        end,
-        ["markdown"] = { { "prettierd", "prettier" } },
-        ["markdown.mdx"] = { { "prettierd", "prettier" } },
-        ["_"] = { "trim_whitespace" },
-      },
-    },
-  },
-  {
     "Wansmer/treesj",
     keys = {
       { "<leader>m", "<CMD>TSJToggle<CR>", desc = "Toggle split/join" },

491adfec1bc994bdc1633e86844dffde33bc537a
Aidan De Angelis <angaidan@amazon.com>
2024-09-13 14:58:04 -0700

    remove trailing whitespace and newlines by default

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,5 +1,18 @@
 return {
   {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        -- Use the "*" filetype to run formatters on all filetypes.
+        ["*"] = { "trim_whitespace" },
+        -- Use the "_" filetype to run formatters on filetypes that don't
+        -- have other formatters configured.
+        ["_"] = { "trim_whitespace", "trim_newlines" },
+      },
+    },
+  },
+  {
     "Wansmer/treesj",
     keys = {
       { "<leader>m", "<CMD>TSJToggle<CR>", desc = "Toggle split/join" },

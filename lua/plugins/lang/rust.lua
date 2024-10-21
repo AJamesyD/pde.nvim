b9fbd20d0856dca1be7c7a2e596dbe69df75d057
Aidan De Angelis <angaidan@amazon.com>
2024-10-20 23:41:01 -0700

    rustconf

--- lua/plugins/lang/rust.lua
+++ lua/plugins/lang/rust.lua
@@ -1,3 +1,24 @@
+---@type rustaceanvim.Executor
+local lazyterm_executor = {
+  execute_command = function(command, args, cwd, _)
+    local shell = require("rustaceanvim.shell")
+
+    local cmd = shell.make_command_from_args(command, args)
+    local cmd = cmd:split(" ")
+    cmd = vim.tbl_filter(function(str)
+      return str ~= nil and #str > 0
+    end, cmd)
+    for i, str in ipairs(cmd) do
+      print("cmd part " .. i .. ": " .. str)
+    end
+    ---@type LazyTermOpts
+    local term_opts = {
+      cwd = cwd,
+    }
+    LazyVim.terminal(cmd, term_opts)
+  end,
+}
+
 return {
   {
     "neovim/nvim-lspconfig",
@@ -45,17 +66,46 @@ return {
         hover_actions = {
           replace_builtin_hover = false,
         },
-        code_actions = {
-          ui_select_fallback = true,
-        },
+        executor = lazyterm_executor,
+        test_executor = "background",
+        crate_test_executor = "background",
       },
       server = {
+        on_attach = function(_, bufnr)
+          local map = MyUtils.map
+
+          map("n", "<leader>dr", function()
+            vim.cmd.RustLsp("debuggables")
+          end, { desc = "Rust Debuggables", buffer = bufnr })
+
+          -- Move lines
+          map("i", "<A-j>", function()
+            vim.cmd.RustLsp({ "moveItem", "down" })
+          end, { desc = "Move Down", buffer = bufnr })
+          map("i", "<A-k>", function()
+            vim.cmd.RustLsp({ "moveItem", "up" })
+          end, { desc = "Move Up", buffer = bufnr })
+          map("v", "<A-j>", function()
+            vim.cmd.RustLsp({ "moveItem", "down" })
+          end, { desc = "Move Down", buffer = bufnr })
+          map("v", "<A-k>", function()
+            vim.cmd.RustLsp({ "moveItem", "up" })
+          end, { desc = "Move Up", buffer = bufnr })
+
+          -- Join lines
+          map("n", "J", function()
+            vim.cmd.RustLsp("joinLines")
+          end, { desc = "Join Lines", buffer = bufnr })
+        end,
         default_settings = {
           -- rust-analyzer language server configuration
           ["rust-analyzer"] = {
             cargo = {
               features = "all",
             },
+            -- TODO: conditionally disable for big projects
+            -- use bacon or flycheck instead
+            checkOnSave = true,
             check = {
               command = "clippy",
               features = "all",
@@ -101,8 +151,8 @@ return {
             return {
               kind.Variable,
               kind.EnumMember,
-              kind.Method,
               kind.Field,
+              kind.Method,
               kind.Function,
             }
           end,

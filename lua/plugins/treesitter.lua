323accbed09aabff96c43a65db0d8cc443adc7b0
Aidan De Angelis <angaidan@amazon.com>
2024-10-25 08:55:00 -0700

    updated treesitter textobjs

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -27,11 +27,37 @@ return {
               ["ia"] = { query = "@parameter.inner", desc = "argument" },
               ["a="] = { query = "@assignment.outer", desc = "assignment" },
               ["i="] = { query = "@assignment.inner", desc = "assignment" },
+              ["l="] = { query = "@assignment.lhs", desc = "assignment" },
+              ["r="] = { query = "@assignment.rhs", desc = "assignment" },
             },
           },
           move = {
             enable = true,
             set_jumps = true,
+            goto_next_start = {
+              ["]f"] = { query = "@function.outer", desc = "Next Function" },
+              ["]c"] = { query = "@class.outer", desc = "Next Class" },
+              ["]a"] = { query = "@parameter.inner", desc = "Next Parameter" },
+              ["]z"] = { query = "@fold", desc = "Next Fold" },
+            },
+            goto_next_end = {
+              ["]F"] = { query = "@function.outer", desc = "Next Function end" },
+              ["]C"] = { query = "@class.outer", desc = "Next Class end" },
+              ["]A"] = { query = "@parameter.inner", desc = "Next Parameter end" },
+              ["]z"] = { query = "@fold", desc = "Next Fold end" },
+            },
+            goto_previous_start = {
+              ["[f"] = { query = "@function.outer", desc = "Prev Function" },
+              ["[c"] = { query = "@class.outer", desc = "Prev Class" },
+              ["[a"] = { query = "@parameter.inner", desc = "Prev Parameter" },
+              ["[z"] = { query = "@fold", desc = "Next Fold" },
+            },
+            goto_previous_end = {
+              ["[F"] = { query = "@function.outer", desc = "Prev Function end" },
+              ["[C"] = { query = "@class.outer", desc = "Prev Class end" },
+              ["[A"] = { query = "@parameter.inner", desc = "Prev Parameter end" },
+              ["[Z"] = { query = "@fold", desc = "Prev Fold end" },
+            },
           },
           swap = {
             enable = true,
@@ -40,8 +66,8 @@ return {
               [">a"] = { query = "@parameter.inner", desc = "Swap next argument" },
             },
             swap_previous = {
-              ["<f"] = { query = "@function.outer", desc = "Swap previous function" },
-              ["<a"] = { query = "@parameter.inner", desc = "Swap previous argument" },
+              ["<f"] = { query = "@function.outer", desc = "Swap prev function" },
+              ["<a"] = { query = "@parameter.inner", desc = "Swap prev argument" },
             },
           },
         },
@@ -50,13 +76,70 @@ return {
       opts = vim.tbl_deep_extend("force", opts, overrides)
       local ensure_installed = {
         "comment",
+        "devicetree", -- For qmk.nvim
         "ini",
         "kdl",
+        "sql",
         "tmux",
       }
       opts.ensure_installed = vim.list_extend(opts.ensure_installed or {}, ensure_installed)
       return opts
     end,
+  },
+  {
+    "chrisgrieser/nvim-various-textobjs",
+    lazy = false,
+    keys = {
+      {
+        "ig",
+        mode = { "o", "x" },
+        function()
+          require("various-textobjs").greedyOuterIndentation("inner")
+        end,
+        desc = "Indentation (greedy)",
+      },
+      {
+        "ag",
+        mode = { "o", "x" },
+        function()
+          require("various-textobjs").greedyOuterIndentation("outer")
+        end,
+        desc = "Indentation (greedy)",
+      },
+      {
+        "iS",
+        mode = { "o", "x" },
+        function()
+          require("various-textobjs").subword("inner")
+        end,
+        desc = "Subword",
+      },
+      {
+        "aS",
+        mode = { "o", "x" },
+        function()
+          require("various-textobjs").subword("outer")
+        end,
+        desc = "Subword",
+      },
+      {
+        "gG",
+        mode = { "o", "x" },
+        function()
+          require("various-textobjs").entireBuffer()
+        end,
+        desc = "File",
+      },
+      {
+        "|",
+        mode = { "o", "x" },
+        function()
+          require("various-textobjs").column()
+        end,
+        desc = "Column",
+      },
+    },
+    opts = {},
   },
   {
     "m-demare/hlargs.nvim",

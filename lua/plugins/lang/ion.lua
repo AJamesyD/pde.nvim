aad06b73111dcd2cafc9f3c80a307585c9a84685
Aidan De Angelis <angaidan@amazon.com>
2024-12-12 21:52:28 +0000

    more amazon conf

---
+++ lua/plugins/lang/ion.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.highlight = opts.highlight or {}
+      local vim_regex_highlighting = opts.highlight.additional_vim_regex_highlighting
+      if type(vim_regex_highlighting) == "table" then
+        vim_regex_highlighting = vim.list_extend(vim_regex_highlighting, { "ion" })
+      else
+        vim_regex_highlighting = { "ion" }
+      end
+
+      opts.highlight.additional_vim_regex_highlighting = vim_regex_highlighting
+      return opts
+    end,
+  },
+}

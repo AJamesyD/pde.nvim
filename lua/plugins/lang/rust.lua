934c1497e53cda621dd7ac647681fdd50221f98b
Aidan De Angelis <angaidan@amazon.com>
2024-12-22 15:24:47 +0000

    remove build dir from rust-analyzer

--- lua/plugins/lang/rust.lua
+++ lua/plugins/lang/rust.lua
@@ -1,24 +1,3 @@
----@type rustaceanvim.Executor
-local terminal_executor = {
-  execute_command = function(command, args, cwd, _)
-    local shell = require("rustaceanvim.shell")
-
-    local cmd = shell.make_command_from_args(command, args)
-    local cmd = cmd:split(" ")
-    cmd = vim.tbl_filter(function(str)
-      return str ~= nil and #str > 0
-    end, cmd)
-    for i, str in ipairs(cmd) do
-      print("cmd part " .. i .. ": " .. str)
-    end
-    ---@type snacks.terminal.Opts
-    local term_opts = {
-      cwd = cwd,
-    }
-    Snacks.terminal.open(cmd, term_opts)
-  end,
-}
-
 return {
   -- Reconfigure LazyVim defaults
   {
@@ -80,166 +59,185 @@ return {
     "mrcjkb/rustaceanvim",
     optional = true,
     version = "^5",
-    ---@type rustaceanvim.Opts
-    opts = {
-      tools = {
-        hover_actions = {
-          replace_builtin_hover = false,
+    ---@param opts rustaceanvim.Opts
+    opts = function(_, opts)
+      ---@type rustaceanvim.Opts
+      local overrides = {
+        tools = {
+          hover_actions = {
+            replace_builtin_hover = false,
+          },
+          ---@type rustaceanvim.FloatWinConfig|vim.api.keyset.win_config
+          float_win_config = {
+            auto_focus = true,
+            open_split = "vertical",
+            width = 0.8,
+          },
+          test_executor = "background",
+          crate_test_executor = "background",
         },
-        ---@type rustaceanvim.FloatWinConfig|vim.api.keyset.win_config
-        float_win_config = {
-          auto_focus = true,
-          open_split = "vertical",
-          width = 0.8,
-        },
-        executor = terminal_executor,
-        test_executor = "background",
-        crate_test_executor = "background",
-      },
-      ---@type rustaceanvim.lsp.ClientConfig
-      ---@diagnostic disable-next-line: missing-fields
-      server = {
-        on_attach = function(client, bufnr)
-          local capability_overrides = {
-            textDocument = {
-              foldingRange = {
-                dynamicRegistration = false,
-                lineFoldingOnly = true,
+        ---@type rustaceanvim.lsp.ClientConfig
+        ---@diagnostic disable-next-line: missing-fields
+        server = {
+          on_attach = function(client, bufnr)
+            local capability_overrides = {
+              textDocument = {
+                foldingRange = {
+                  dynamicRegistration = false,
+                  lineFoldingOnly = true,
+                },
               },
-            },
-          }
-          client.capabilities = vim.tbl_deep_extend("force", client.capabilities or {}, capability_overrides)
-          local map = MyUtils.map
+            }
+            client.capabilities = vim.tbl_deep_extend("force", client.capabilities or {}, capability_overrides)
+            local map = MyUtils.map
 
-          map("n", "<leader>dr", function()
-            vim.cmd.RustLsp("debuggables")
-          end, { desc = "Rust Debuggables", buffer = bufnr })
+            map("n", "<leader>dr", function()
+              vim.cmd.RustLsp("debuggables")
+            end, { desc = "Rust Debuggables", buffer = bufnr })
 
-          -- Move lines
-          map("i", "<A-j>", function()
-            vim.cmd.RustLsp({ "moveItem", "down" })
-          end, { desc = "Move Down", buffer = bufnr })
-          map("i", "<A-k>", function()
-            vim.cmd.RustLsp({ "moveItem", "up" })
-          end, { desc = "Move Up", buffer = bufnr })
-          map("v", "<A-j>", function()
-            vim.cmd.RustLsp({ "moveItem", "down" })
-          end, { desc = "Move Down", buffer = bufnr })
-          map("v", "<A-k>", function()
-            vim.cmd.RustLsp({ "moveItem", "up" })
-          end, { desc = "Move Up", buffer = bufnr })
+            -- Move lines
+            map("i", "<A-j>", function()
+              vim.cmd.RustLsp({ "moveItem", "down" })
+            end, { desc = "Move Down", buffer = bufnr })
+            map("i", "<A-k>", function()
+              vim.cmd.RustLsp({ "moveItem", "up" })
+            end, { desc = "Move Up", buffer = bufnr })
+            map("v", "<A-j>", function()
+              vim.cmd.RustLsp({ "moveItem", "down" })
+            end, { desc = "Move Down", buffer = bufnr })
+            map("v", "<A-k>", function()
+              vim.cmd.RustLsp({ "moveItem", "up" })
+            end, { desc = "Move Up", buffer = bufnr })
 
-          -- Join lines
-          map("n", "J", function()
-            vim.cmd.RustLsp("joinLines")
-          end, { desc = "Join Lines", buffer = bufnr })
+            -- Join lines
+            map("n", "J", function()
+              vim.cmd.RustLsp("joinLines")
+            end, { desc = "Join Lines", buffer = bufnr })
 
-          -- HACK: Workaround to ignore ServerCancelled error
-          -- https://github.com/neovim/neovim/issues/30985#issuecomment-2447329525
-          for _, method in ipairs({ "textDocument/diagnostic", "workspace/diagnostic" }) do
-            local default_diagnostic_handler = vim.lsp.handlers[method]
-            vim.lsp.handlers[method] = function(err, result, context, config)
-              if err ~= nil and err.code == -32802 then
-                return
+            -- HACK: Workaround to ignore ServerCancelled error
+            -- https://github.com/neovim/neovim/issues/30985#issuecomment-2447329525
+            for _, method in ipairs({ "textDocument/diagnostic", "workspace/diagnostic" }) do
+              local default_diagnostic_handler = vim.lsp.handlers[method]
+              vim.lsp.handlers[method] = function(err, result, context, config)
+                if err ~= nil and err.code == -32802 then
+                  return
+                end
+                return default_diagnostic_handler(err, result, context, config)
               end
-              return default_diagnostic_handler(err, result, context, config)
             end
-          end
-        end,
+          end,
 
-        default_settings = {
-          -- rust-analyzer language server configuration
-          ["rust-analyzer"] = {
-            assist = {
-              emitMustUse = true,
-            },
-            cargo = {
-              features = "all",
-            },
-            -- TODO: conditionally disable for big projects
-            -- use bacon or flycheck instead
-            checkOnSave = true,
-            check = {
-              command = "clippy",
-              extraArgs = {
-                "--no-deps",
+          default_settings = {
+            -- rust-analyzer language server configuration
+            ["rust-analyzer"] = {
+              assist = {
+                emitMustUse = true,
               },
-              features = "all",
-            },
-            completion = {
-              fullFunctionSignatures = { enable = true },
-              limit = 100,
-              postfix = { enable = false }, -- TODO: eventually try using these
-              termSearch = { enable = true }, -- TODO: experiment
-            },
-            diagnostics = {
-              enable = true,
-              experimental = true,
-              disabled = {
-                "unresolved-proc-macro",
+              cargo = {
+                features = "all",
               },
-              styleLints = { enable = true },
-            },
-            hover = {
-              actions = {
-                references = { enable = true },
+              -- TODO: conditionally disable for big projects
+              -- use bacon or flycheck instead
+              checkOnSave = true,
+              check = {
+                command = "clippy",
+                extraArgs = {
+                  "--no-deps",
+                },
+                features = "all",
               },
-              show = {
-                enumVariants = 10,
-                fields = 10,
+              completion = {
+                fullFunctionSignatures = { enable = true },
+                limit = 100,
+                postfix = { enable = false }, -- TODO: eventually try using these
+                termSearch = { enable = true }, -- TODO: experiment
               },
-            },
-            -- XXX: Should inherit .rustfmt config when available
-            imports = {
-              granularity = { enforce = true },
-              preferPrelude = true,
-            },
-            inlayHints = {
-              closingBraceHints = {
+              diagnostics = {
                 enable = true,
-                minLines = 35,
+                experimental = true,
+                disabled = {
+                  "unresolved-proc-macro",
+                },
+                styleLints = { enable = true },
               },
-              -- closureCaptureHints = { enable = true }, -- TODO: make toggle-able
-              closureReturnTypeHints = { enable = "block" },
-              expressionAdjustmentHints = {
+              files = {
+                excludeDirs = {
+                  ".direnv",
+                  ".git",
+                  ".github",
+                  ".gitlab",
+                  "bin",
+                  "build",
+                  "node_modules",
+                  "target",
+                  "venv",
+                  ".venv",
+                },
+              },
+              hover = {
+                actions = {
+                  references = { enable = true },
+                },
+                show = {
+                  enumVariants = 10,
+                  fields = 10,
+                },
+              },
+              -- XXX: Should inherit .rustfmt config when available
+              imports = {
+                granularity = { enforce = true },
+                preferPrelude = true,
+              },
+              inlayHints = {
+                closingBraceHints = {
+                  enable = true,
+                  minLines = 35,
+                },
+                -- closureCaptureHints = { enable = true }, -- TODO: make toggle-able
+                closureReturnTypeHints = { enable = "block" },
+                expressionAdjustmentHints = {
+                  enable = true,
+                  hideOutsideUnsafe = true,
+                },
+                genericParameterHints = {
+                  lifetime = { enable = true },
+                  type = { enable = true },
+                },
+              },
+              lens = {
                 enable = true,
-                hideOutsideUnsafe = true,
+                debug = { enable = false }, -- TODO: find better way to integrate with nvim-dap
+                implementations = { enable = true },
+                -- All references default to false.
+                -- TODO: make toggle-able
+                references = {
+                  -- adt = { enable = true },
+                  -- enumVariant = { enable = true },
+                  -- method = { enable = true },
+                  -- trait = { enable = true },
+                },
+                run = { enable = false }, -- TODO: find better way to integrate with neotest
               },
-              genericParameterHints = {
-                lifetime = { enable = true },
-                type = { enable = true },
+              lru = {
+                capacity = 512,
               },
-            },
-            lens = {
-              enable = true,
-              debug = { enable = false }, -- TODO: find better way to integrate with nvim-dap
-              implementations = { enable = true },
-              -- All references default to false.
-              -- TODO: make toggle-able
-              references = {
-                -- adt = { enable = true },
-                -- enumVariant = { enable = true },
-                -- method = { enable = true },
-                -- trait = { enable = true },
+              rustfmt = {
+                extraArgs = "+nightly",
               },
-              run = { enable = false }, -- TODO: find better way to integrate with neotest
-            },
-            lru = {
-              capacity = 512,
-            },
-            rustfmt = {
-              extraArgs = "+nightly",
-            },
-            workspace = {
-              symbol = {
-                search = {
-                  limit = 512,
+              workspace = {
+                symbol = {
+                  search = {
+                    limit = 512,
+                  },
                 },
               },
             },
           },
         },
-      },
-    },
+      }
+
+      opts = vim.tbl_deep_extend("force", opts, overrides)
+      return opts
+    end,
   },
 }

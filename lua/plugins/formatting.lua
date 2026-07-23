ac72e5f4cbf714c20571ccfbc68d94e2ac79b2e4
Aidan De Angelis <aidandeangelis@berkeley.edu>
2026-07-23 19:21:06 +0000

    fix(formatting): remove injected formatter from auto-format
    The injected formatter runs shfmt on bash inside YAML block scalars,
    stripping indentation required by >- and | syntax. Use <leader>cF for
    on-demand injected formatting instead.

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -17,7 +17,7 @@ return {
       formatters_by_ft = {
         -- TODO: Figure out how to make * and _ respect vim.b/g.autoformat
         -- Use the "*" filetype to run formatters on all filetypes.
-        ["*"] = { "trim_whitespace", "injected" },
+        ["*"] = { "trim_whitespace" },
         -- Use the "_" filetype to run formatters on filetypes that don't
         -- have other formatters configured.
         -- ["_"] = { "trim_newlines" },
